26c2d1e9a25a63d62c10d899c4b6595a04771c16
lucashellanio <31229434+lucashellanio@users.noreply.github.com>
2022-05-21 18:59:27 -0300

    Master | FIX update exempol in TS

--- Lucas - Adapter/index.ts
+++ Lucas - Adapter/index.ts
@@ -1,3 +1,10 @@
+// Exemplo conceitual
+// Este exemplo ilustra a estrutura do padrão de projeto Adapter. Ele se concentra em responder a estas perguntas:
+
+// De quais classes ele consiste?
+// Quais papéis essas classes desempenham?
+// De que maneira os elementos do padrão estão relacionados?
+
 /**
  * The Target defines the domain-specific interface used by the client code.
  */
@@ -58,3 +65,13 @@ console.log('');
 console.log('Client: But I can work with it via the Adapter:');
 const adapter = new Adapter(adaptee);
 clientCode(adapter);
+
+// Output Resultados da execução
+// Client: I can work just fine with the Target objects:
+// Target: The default target's behavior.
+
+// Client: The Adaptee class has a weird interface. See, I don't understand it:
+// Adaptee: .eetpadA eht fo roivaheb laicepS
+
+// Client: But I can work with it via the Adapter:
+// Adapter: (TRANSLATED) Special behavior of the Adaptee.
